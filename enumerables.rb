dfb7ef2dc5e276387156b49ff83588e57dab7187
Thant Ko Zaw <thantmkou10@gmail.com>
2021-09-08 10:37:58 +0630

    Refactor all methods for efficiency

--- enumerables.rb
+++ enumerables.rb
@@ -31,37 +31,27 @@ module Enumerable
     # Below methods will work with only arrays but will not work if the array contains
     # nil value
   def my_all?(argv=nil, &block)
-    match_all = true
     block = Proc.new { |item| item unless item.nil? || !item } unless block_given?
     block = Proc.new { |item| item if argv === item} unless argv.nil?
-    self.my_each { |item| match_all = false unless block.call(item) }
-    match_all
+    self.my_each { |item| return false unless block.call(item) }
+    
+    true
   end
 
   def my_any?(argv=nil, &block)
-    match_any = false
     block = Proc.new { |item| item unless item.nil? || !item } unless block_given?
     block = Proc.new { |item| item if argv === item} unless argv.nil?
-    self.my_each do |item|
-      if block.call(item)
-        match_any = true
-        break
-      end
-    end
-    match_any
+    self.my_each { |item| return true if block.call(item)}
+
+    false
   end
 
   def my_none?(argv=nil, &block)
-    match_none = true
     block = Proc.new { |item| item unless item.nil? || !item } unless block_given?
     block = Proc.new { |item| item if argv === item} unless argv.nil?
-    self.my_each do |item|
-      if block.call(item)
-        match_none = false
-        break
-      end
-    end
-    match_none
+    self.my_each { |item| return false if block.call(item)}
+
+    true
   end
 
   def my_count(argv=nil, &block)
